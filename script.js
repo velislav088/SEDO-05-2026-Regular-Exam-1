8749d859905fed5233ce14a1dfafd4158deb7705
velislav088 <156554141+velislav088@users.noreply.github.com>
2026-06-27 10:37:33 +0300

    Add random color button

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.getElementById('apply-btn').addEventListener('click', function () {
   var c = document.getElementById('color-input').value;
   document.getElementById('box').style.backgroundColor = c;
 });
+
+document.getElementById('random-btn').addEventListener('click', function () {
+  var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+  document.getElementById('color-input').value = randomColor;
+  document.getElementById('box').style.backgroundColor = randomColor;
+});
